95b8c22928360d7758e8c87532b81e962ec7d3a3
jinal Patel <jinalptl97@gmail.com>
2022-12-01 18:15:05 +0000

    Commit on 1st decembet

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.kotlin_practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val message = findViewById<TextView>(R.id.message)
+        val recycleview=findViewById<RecyclerView>(R.id.recycleview)
+        recycleview.layoutManager=LinearLayoutManager(this)
         message.text = "Hello world"
+
+
+        Toast.makeText(this,"Hello Jinal", Toast.LENGTH_SHORT).show()
     }
 }
